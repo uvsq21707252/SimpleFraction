460c7ce467e941c190fa263ea37c162783bd1e3d
Raphaël Marouani <raphael@marouani.fr>
2020-10-04 22:21:52 +0200

    Commit 2

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args){
+
+		System.out.println("Voici la Fraction: \n");
+
+		fraction f = new fraction("1", "1", "2");
+
+		System.out.println( f.toString());
+		
+	}
+}
