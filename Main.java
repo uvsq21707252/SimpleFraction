efec41f12864ea4fe1af0f14a57aa07f730c8b43
Raphaël Marouani <raphael@marouani.fr>
2020-10-04 23:16:45 +0200

    third.commit

--- Main.java
+++ Main.java
@@ -2,13 +2,11 @@ public class Main {
 
 	public static void main(String[] args){
 
-		int n, d;
+		fraction M = new fraction(2,3);
 
-		n = 8; d = 9;
-
-		System.out.println(" fraction = " + n + "/" + d + "\n");
-
+		System.out.println(" fraction 1 =" + M.toString() + "\n");
 
 	}
+
 }
 
--- fraction.java
+++ fraction.java
@@ -1,2 +1,21 @@
+public class fraction {
 
+	private int a,b;
+
+	public fraction(int a, int b){
+
+		this.a = a;
+		this.b = b;
+
+	}
+
+	public String toString(){
+
+		return a + "/" + b;
+
+	}
+
+
+
+}
 
